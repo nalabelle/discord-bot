3885b185443ef1232a4e7c7705e75e81e1115763
nalabelle <nalabelle@gmail.com>
2020-08-24 18:14:52 -0700

    add repost logging

--- extensions/repost/cmd.py
+++ extensions/repost/cmd.py
@@ -1,7 +1,10 @@
+import logging
 import discord
 from discord.ext import commands
 from pathlib import Path
 from .repost import Repost,RepostData
+
+log = logging.getLogger('extRepost')
 
 class RepostCommand(commands.Cog, name="Repost"):
     def __init__(self, bot):
@@ -10,6 +13,7 @@ class RepostCommand(commands.Cog, name="Repost"):
         self.data = RepostData().from_yaml(path=path)
         self.listen_channels = dict()
         self.load_channels()
+        log.info("Loaded {} channels".format(len(self.listen_channels)))
 
     def load_channels(self):
         for r in self.data.subscriptions:
@@ -31,15 +35,24 @@ class RepostCommand(commands.Cog, name="Repost"):
             self.load_channels()
             await ctx.message.channel.send('Subscribed {} to {}'.format(source, dest))
 
+    @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
+        if self.bot.user.id == message.author.id:
+            return True
         r = self.listen_channels.get(message.channel.id)
+        log.debug("Checking for channel {}".format(r))
         if r is not None:
             for channel in r:
-                content = message.content
-                content = content + '\n```{}```'.format(message.jump_url)
+                log.debug("Posting to channel {}".format(channel))
+                embed = discord.Embed.from_dict({
+                    "description": message.content,
+                    "url": message.jump_url
+                    })
+                author_name = message.author.nick or message.author.name
+                embed.set_author(name=author_name,url=message.jump_url,icon_url=message.author.avatar_url)
                 chan = discord.utils.get(self.bot.get_all_channels(),
-                        id=r.dest_channel)
-                chan.send(content)
+                        id=channel)
+                await chan.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(RepostCommand(bot))
