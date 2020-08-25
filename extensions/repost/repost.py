a27b9d1c7b41c8aace8e15a3644ae806b72b8505
nalabelle <nalabelle@gmail.com>
2020-08-24 17:48:45 -0700

    add cmp function for repost configs

--- extensions/repost/repost.py
+++ extensions/repost/repost.py
@@ -7,6 +7,12 @@ class Repost(Data):
     source_channel: str
     dest_channel: str
 
+    def __eq__(self, other):
+        if self.source_channel == other.source_channel:
+            if self.dest_channel == other.dest_channel:
+                return True
+        return False
+
 @dataclass
 class RepostData(DataFile):
     subscriptions: List[Repost] = field(default_factory=list)
