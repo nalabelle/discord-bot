8f514b9d12373ef0d63c6a18c1d5ecf582db3d9c
nalabelle <nalabelle@gmail.com>
2021-02-21 15:10:35 -0800

    fix weather tests

--- extensions/weather/test_service.py
+++ extensions/weather/test_service.py
@@ -1,22 +1,24 @@
 import json
+from unittest.mock import patch
 from .service import WeatherService, Alert
 from forecastio.models import Forecast
 from geocoder.google import GoogleResult
-weather_service = WeatherService()
 
-testdata = None
-f = open("weather/api_test_response.txt", "r")
-testdata = f.read()
-f.close()
+@patch('file_secrets.file_secrets.FileSecrets.get')
+def test_weather(Secrets):
+    weather_service = WeatherService()
 
-test_forecast = Forecast(json.loads(testdata), None, None)
+    testdata = None
+    f = open("weather/api_test_response.txt", "r")
+    testdata = f.read()
+    f.close()
 
-f = open("weather/api_geocode_test.txt", "r")
-testdata = f.read()
-test_geocode = GoogleResult(json.loads(testdata)["results"][0])
+    test_forecast = Forecast(json.loads(testdata), None, None)
 
+    f = open("weather/api_geocode_test.txt", "r")
+    testdata = f.read()
+    test_geocode = GoogleResult(json.loads(testdata)["results"][0])
 
-def test_weather():
     weather = weather_service.forecast_to_object(test_geocode.address, test_forecast)
     assert weather is not None, "Weather Service Works"
 
